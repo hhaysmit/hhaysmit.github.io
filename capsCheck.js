df33f37271047e5291d2359f2d5c1ea24cb929c4
Hannah Hay-Smith <hannah_hay-smith@brown.edu>
2015-06-15 16:40:44 -0700

    Caps Lock

---
+++ capsCheck.js
@@ -0,0 +1,29 @@
+var checkCaps = function(field, x, y){
+	var warned = false; 
+	field.addEventListener("keydown", function(event){
+		if(field.value.length > 2){
+		var caps = /^[^a-z]*$/ //Checks for lower case letters
+		var numbers = /^[0-9\W]*$/ //Checks if string is only numbers or special characters
+		if(caps.test(field.value) && !numbers.test(field.value) && !warned){
+			var svg= d3.select("body").append("svg")
+				.attr("x", x)
+				.attr("y", y);
+			var text = svg.append("text")
+				.text("Warning: Caps lock is on")
+				.attr("x", x)
+				.attr("y", y)
+				.attr("font-size", "11px")
+				.attr("font-family", "sans-serif");
+			warned = true; 
+		}
+
+		if(warned){
+			if(/[a-z]+/.test(field.value))
+				d3.select("text").remove()
+				warned = false
+		}
+
+	}
+})
+}
+	
